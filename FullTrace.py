e60c8d9df7f56ac6f4dc03c21e2a723e56d2c187
Michel <chmichel66@hotmail.fr>
2016-05-18 16:11:46 +0100

    Update FullTrace.py

--- FullTrace.py
+++ FullTrace.py
@@ -11,23 +11,25 @@ from scipy.integrate import odeint
 from numpy.random import *
 ion()
 
-# these two curves are called characteristics curves (activation and kinetics against voltage)
-# and model the activation voltage of channels
-def ActivationCurve(V, Vm, k):
-  return 1/(1+exp(-(V-Vm)/k))
-  
-def Kinetic(V, Vmax, sigma, Camp, Cbase):
+# these two curves are called characteristics curves and model the activation...
+def ActivationCurve(V, Vh, k):
+  # Vh is the half activation, k is the slope
+  return 1/(1+exp(-(V-Vh)/k))
+
+# ... and the kinetics of the ionic channel against voltage
+def Kinetic(V, Vmax, sigma, Camp, Cbase): 
+  # bell-shaped curve centred in Vmax, with amplitude Camp, width sigma, and offset Cbase
   return Cbase + Camp*exp(-(Vmax-V)**2/sigma**2)
 
-# this equation modelize the opening of a gate, given the membrane potential and the two precedent equations
+# this equation modelize the opening of a channel, given the membrane potential and the two precedent equations
 def Gate(x,t):
   global pars
   xinf = ActivationCurve(V[:,1], pars[0], pars[1])
   tau = Kinetic(V[:,1], pars[2], pars[3], pars[4], pars[5])
   return (xinf-x)/tau
 
-# the following function is the optimisation part of the algorithm. The two chanels are modeled and the result is compared
-# to the data, taking account the parameter physiological ranges for identification
+# the following function is the optimisation part of the algorithm. The two chanels are modeled with initial conditions 
+# and the result is compared to the data, taking account the parameter physiological ranges for identification
 def FullTrace(p,time,y):
   global pars
   pars = p[0:6]
@@ -37,9 +39,13 @@ def FullTrace(p,time,y):
   rinit2 = ActivationCurve(V[:,0], pars[0], pars[1]) # initial values
   r2 = odeint(Gate,rinit2,time)
   I = p[6]*r1*(V[:,1]-E)+p[13]*r2*(V[:,1]-E)
+  
+  # the following plot is to see in real time the reconstructed curves trying to fit the data (very fun)
   plot(time,y,'b'); hold(True)
   plot(time,I,'--r');hold(False)
   draw()
+  
+  # this part is added to constrain the algorithm with physiological search ranges
   A = 0
   for a in arange(len(p)):
     if p[a] > HB[a] :
@@ -81,36 +87,40 @@ y = par1[6]*x1*(V[:,1]-E)+par2[6]*x2*(V[:,1]-E)+10*randn(len(time),len(V))
 # eventual local minima
 for NbRandTries in arange(0,10):
   P = par1+par2
-  tol = 0.01
+  # here the search ranges are around the actual parameter vetor with a tolerance of +/-50%
+  tol = 0.50
   LB = [(1-tol)*x for x in P]
   HB = [(1+tol)*x for x in P]
   p0 = LB+rand(len(P))*(array(HB)-array(LB))
 
-  # results are plotted for each individual try
+  # optimisation function with real time display of the minimization on the...
   figure(0)
   parId = fmin_powell(FullTrace, p0, args=(time,y),maxfun = 5000)
   print parId
 
+  # results are plotted for each individual try
   figure(1,facecolor=[1,1,1])
   Vplot = arange(-180,-50)
   
   # for the activation curve
   subplot(2,1,1)
   hold(True)
-  # for each Ih component
+  # for activation 1...
   plot(Vplot,ActivationCurve(Vplot, parId[0], parId[1]),'k',linewidth = 0.5)
+  # ... and activation 2
   plot(Vplot,ActivationCurve(Vplot, parId[7], parId[8]),'k',linewidth = 0.5)
   
   # and for the kinetic curve
   subplot(2,1,2)
   hold(True)
-  # for each Ih component
+  # for kinetic 1...
   plot(Vplot,Kinetic(Vplot, parId[2], parId[3], parId[4], parId[5]),'k',linewidth = 0.5)
+  # ... and kinetic 2
   plot(Vplot,Kinetic(Vplot, parId[9], parId[10], parId[11], parId[12]),'k',linewidth = 0.5)
   
   draw()
 
-# the good results (determined after first mode of the goodness of fit histogram) are averaged and plotted
+# the good results (determined from first mode of the goodness of fit histogram) are averaged and plotted
 subplot(2,1,1)
 hold(True)
 plot(Vplot,ActivationCurve(Vplot, par1[0], par1[1]),'r')
